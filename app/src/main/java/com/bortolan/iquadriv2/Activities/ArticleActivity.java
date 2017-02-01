2e0672543bb19bb6c192b419ee0a8653860eda12
Marco <bortolanmarco@gmail.com>
2017-02-01 01:30:53 +0100

    gradient

--- app/src/main/java/com/bortolan/iquadriv2/Activities/ArticleActivity.java
+++ app/src/main/java/com/bortolan/iquadriv2/Activities/ArticleActivity.java
@@ -1,6 +1,7 @@
 package com.bortolan.iquadriv2.Activities;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
@@ -19,6 +20,8 @@ public class ArticleActivity extends AppCompatActivity {
     ImageView image;
     @BindView(R.id.content)
     TextView content;
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +34,16 @@ public class ArticleActivity extends AppCompatActivity {
         new DownloadArticle(article -> {
             Picasso.with(this).load(article.getImage()).into(image);
             content.setText(article.getBody());
-            setTitle(article.getTitle());
+            setCollapsingToolbarLayoutTitle(article.getTitle());
         }).execute(getIntent().getStringExtra("url"));
 
+    }
+
+    private void setCollapsingToolbarLayoutTitle(String title) {
+        mCollapsingToolbarLayout.setTitle(title);
+        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
+        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBarPlus1);
+        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBarPlus1);
     }
 }
