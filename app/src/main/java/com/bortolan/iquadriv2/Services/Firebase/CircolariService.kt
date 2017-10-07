86b92960fd2b98addc34ddc8ffc63c63c09df1c7
Quix0tic <bortolanmarco@gmail.com>
2017-10-07 23:34:41 +0200

    Notification improvement

---
+++ app/src/main/java/com/bortolan/iquadriv2/Services/Firebase/CircolariService.kt
@@ -0,0 +1,147 @@
+package com.bortolan.iquadriv2.Services.Firebase
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.RingtoneManager
+import android.os.Build
+import android.preference.PreferenceManager
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import android.util.Log
+import com.bortolan.iquadriv2.Activities.ActivityMain
+import com.bortolan.iquadriv2.Interfaces.Circolare
+import com.bortolan.iquadriv2.R
+import com.bortolan.iquadriv2.Tasks.Cache.CacheListTask
+import com.bortolan.iquadriv2.Tasks.Remote.DownloadCircolari
+import com.bortolan.iquadriv2.Utils.Methods
+import com.firebase.jobdispatcher.JobParameters
+import com.firebase.jobdispatcher.JobService
+
+
+class CircolariService : JobService() {
+    override fun onStartJob(job: JobParameters?): Boolean {
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        if (Methods.isNetworkAvailable(applicationContext)) {
+            DownloadCircolari(preferences) { list ->
+                if (list == null || list.isEmpty()) return@DownloadCircolari
+
+                Log.d("CircolariService", list[0].title)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    checkUpdatesV26(applicationContext, list[0], preferences, last_circolare, preferences.getBoolean("notify_circolari", true))
+                } else {
+                    checkUpdates(applicationContext, list[0], preferences, last_circolare, preferences.getBoolean("notify_circolari", true))
+                }
+                CacheListTask(applicationContext.cacheDir, "Circolari").execute(list)
+
+                return@DownloadCircolari
+            }.execute()
+        }
+        return false
+    }
+
+    override fun onStopJob(job: JobParameters?): Boolean {
+        return false
+    }
+
+    @RequiresApi(26)
+    private fun checkUpdatesV26(context: Context, firstItem: Circolare, preferences: SharedPreferences, last_item_key_name: String, notify: Boolean) {
+        if (notify) {
+            //TO-DO: uncomment
+            if (!firstItem.title.toLowerCase().trim().equals(preferences.getString(last_item_key_name, "").toLowerCase().trim())) {
+                Log.w("CircolariService", "Shoot Notification -> " + firstItem.title)
+
+                val notificationManager: NotificationManager = getSystemService(NotificationManager::class.java)
+                val mBuilder: Notification.Builder
+
+                val title = "Quadri - Circolari"
+                val content = "Nuove circolari da leggere"
+                val i = Intent(context, ActivityMain::class.java)
+                i.putExtra("tab", R.id.tab_circolari)
+                val intent = PendingIntent.getActivity(context, ActivityMain.CIRCOLARI_ID, i, 0)
+
+                mBuilder = Notification.Builder(context, if (preferences.getBoolean("notify_sound", true)) channelId else channelId_mute)
+                        .setSmallIcon(R.drawable.ic_stat_name)
+                        .setContentText(content)
+                        .setContentTitle(title)
+                        .setContentIntent(intent)
+                        .setAutoCancel(true)
+
+
+                val channel = NotificationChannel(channelId, "iQuadri", NotificationManager.IMPORTANCE_HIGH)
+                channel.enableLights(true)
+                channel.enableVibration(preferences.getBoolean("notify_vibrate", true))
+                channel.lightColor = Color.BLUE
+
+                val channelMute = NotificationChannel(channelId_mute, "iQuadri silent", NotificationManager.IMPORTANCE_LOW)
+                channelMute.enableLights(true)
+                channelMute.enableVibration(preferences.getBoolean("notify_vibrate", true))
+                channelMute.lightColor = Color.BLUE
+
+                if (preferences.getBoolean("notify_vibrate", true)) {
+                    channel.vibrationPattern = longArrayOf(250, 250, 250, 250)
+                }
+                if (preferences.getBoolean("notify_sound", true)) {
+                    channel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION), AudioAttributes.Builder().setLegacyStreamType(AudioManager.STREAM_NOTIFICATION).build())
+                }
+
+                notificationManager.createNotificationChannel(channel)
+                notificationManager.createNotificationChannel(channelMute)
+                notificationManager.notify(nNotif, mBuilder.build())
+
+                PreferenceManager.getDefaultSharedPreferences(context).edit().putString(last_item_key_name, firstItem.title.toLowerCase().trim { it <= ' ' }).apply()
+            }
+        }
+    }
+
+    private fun checkUpdates(context: Context, firstItem: Circolare, preferences: SharedPreferences, last_item_key_name: String, notify: Boolean) {
+        if (notify) {
+            //TO-DO: uncomment
+            if (!firstItem.title.toLowerCase().trim().equals(preferences.getString(last_item_key_name, "").toLowerCase().trim())) {
+                Log.w("CircolariService", "Shoot Notification -> " + firstItem.title)
+
+                val notificationManager: NotificationManagerCompat = NotificationManagerCompat.from(context)
+                val mBuilder: NotificationCompat.Builder
+
+                val title = "Quadri - Circolari"
+                val content = "Nuove circolari da leggere"
+                val i = Intent(context, ActivityMain::class.java)
+                i.putExtra("tab", R.id.tab_circolari)
+                val intent = PendingIntent.getActivity(context, ActivityMain.CIRCOLARI_ID, i, 0)
+
+                mBuilder = NotificationCompat.Builder(context, "iQuadri")
+                        .setSmallIcon(R.drawable.ic_stat_name)
+                        .setContentText(content)
+                        .setContentTitle(title)
+                        .setContentIntent(intent)
+                        .setLights(Color.BLUE, 3000, 3000)
+                        .setAutoCancel(true)
+
+                if (preferences.getBoolean("notify_vibrate", true))
+                    mBuilder.setVibrate(longArrayOf(250, 250, 250, 250))
+                if (preferences.getBoolean("notify_sound", true))
+                    mBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+
+                notificationManager.notify(nNotif, mBuilder.build())
+
+                PreferenceManager.getDefaultSharedPreferences(context).edit().putString(last_item_key_name, firstItem.title.toLowerCase().trim { it <= ' ' }).apply()
+            }
+        }
+    }
+
+    companion object {
+        private const val nNotif: Int = 977
+        private val last_circolare = "last_circolare"
+        private val channelId = "iquadri_channel_01"
+        private val channelId_mute = "iquadri_channel_02"
+    }
+}
--- app/src/main/java/com/bortolan/iquadriv2/Services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/bortolan/iquadriv2/Services/MyFirebaseMessagingService.kt
@@ -4,46 +4,58 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import android.preference.PreferenceManager
 import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.bortolan.iquadriv2.Activities.ActivityMain
 import com.bortolan.iquadriv2.R
+import com.bortolan.iquadriv2.Services.Firebase.CircolariService
+import com.bortolan.iquadriv2.Services.Firebase.QDSService
+import com.firebase.jobdispatcher.FirebaseJobDispatcher
+import com.firebase.jobdispatcher.GooglePlayDriver
+import com.firebase.jobdispatcher.Trigger
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     var id: Int = 0
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        val data = remoteMessage!!.data
-        Log.w("FIREBASE", "RECEIVE")
+        if (remoteMessage == null) return
+
+        val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(applicationContext))
+        val data = remoteMessage.data
+        Log.w("FIREBASE", "RECEIVE: " + data.toString())
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
 
         if (!data["isbn"].isNullOrEmpty()) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                val intent = Intent(this, ActivityMain::class.java)
+                intent.putExtra("tab", R.id.tab_libri)
+                intent.putExtra("query", data["isbn"].toString())
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                        PendingIntent.FLAG_ONE_SHOT)
+                val builder = NotificationCompat.Builder(applicationContext, "iQuadri").setContentTitle(data["title"].toString()).setContentText(data["body"].toString()).setSmallIcon(R.mipmap.ic_launcher).setLights(Color.argb(255, 0, 0, 240), 1000, 1000).setAutoCancel(true)
+                builder.setContentIntent(pendingIntent)
 
-            val intent = Intent(this, ActivityMain::class.java)
-            intent.putExtra("tab", R.id.tab_libri)
-            intent.putExtra("query", data["isbn"].toString())
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            val pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                    PendingIntent.FLAG_ONE_SHOT)
-            val builder = NotificationCompat.Builder(applicationContext, "iQuadri").setContentTitle(data["title"].toString()).setContentText(data["body"].toString()).setSmallIcon(R.mipmap.ic_launcher).setLights(Color.argb(255, 0, 0, 240), 1000, 1000).setAutoCancel(true)
-            builder.setContentIntent(pendingIntent)
-
-            val mNotificationManager = getSystemService(NotificationManager::class.java)
-            // mId allows you to update the notification later on.
-            id = PreferenceManager.getDefaultSharedPreferences(applicationContext).getInt("notification_id", 0)
-            mNotificationManager.notify(id, builder.build())
-            PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putInt("notification_id", id + 1).apply()
+                val mNotificationManager = getSystemService(NotificationManager::class.java)
+                // mId allows you to update the notification later on.
+                id = PreferenceManager.getDefaultSharedPreferences(applicationContext).getInt("notification_id", 0)
+                mNotificationManager.notify(id, builder.build())
+                PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putInt("notification_id", id + 1).apply()
+            }
         } else if (pref.getBoolean("notify", true)) {
             Log.w("FirebaseService", "Shoot Notification")
 
             if (data["action"]?.equals("circolari") == true && pref.getBoolean("circolari", true)) {
-                startService(Intent(this, CircolariNotification::class.java))
+                val job = dispatcher.newJobBuilder().setService(CircolariService::class.java).setTag("iQuadri_service").setRecurring(false).setTrigger(Trigger.executionWindow(0, 20)).build()
+                dispatcher.mustSchedule(job)
             } else if (data["action"]?.equals("studenti") == true && pref.getBoolean("studenti", true)) {
-                startService(Intent(this, QDSNotification::class.java))
-
+                val job = dispatcher.newJobBuilder().setService(QDSService::class.java).setTag("iQuadri_service").setRecurring(false).setTrigger(Trigger.executionWindow(0, 20)).build()
+                dispatcher.mustSchedule(job)
             }
         }
 
